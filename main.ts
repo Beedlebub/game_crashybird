5781da83bbecbff239bb420f38f3c26745ab3b3f
Beedlebub <beedlebub@gmail.com>
2020-08-25 21:14:14 -0700

    Update main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -4,10 +4,9 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     bird.change(LedSpriteProperty.Y, 1)
 })
-let emptyObstacleY = 0
+let empty_obstacle_y = 0
 let ticks = 0
 let bird: game.LedSprite = null
-let index = 0
 let obstacles: game.LedSprite[] = []
 bird = game.createSprite(0, 2)
 bird.set(LedSpriteProperty.Blink, 300)
@@ -15,19 +14,19 @@ basic.forever(function () {
     while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.X) == 0) {
         obstacles.removeAt(0).delete()
     }
-    for (let obstacle2 of obstacles) {
-        obstacle2.change(LedSpriteProperty.X, -1)
+    for (let obstacle of obstacles) {
+        obstacle.change(LedSpriteProperty.X, -1)
     }
     if (ticks % 3 == 0) {
-        emptyObstacleY = randint(0, 4)
-        for (let index2 = 0; index2 <= 4; index2++) {
-            if (index2 != emptyObstacleY) {
-                obstacles.push(game.createSprite(4, index2))
+        empty_obstacle_y = randint(0, 4)
+        for (let index = 0; index <= 4; index++) {
+            if (index != empty_obstacle_y) {
+                obstacles.push(game.createSprite(4, index))
             }
         }
     }
-    for (let obstacle3 of obstacles) {
-        if (obstacle3.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) && obstacle3.get(LedSpriteProperty.Y) == bird.get(LedSpriteProperty.Y)) {
+    for (let obstacle_collision of obstacles) {
+        if (obstacle_collision.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) && obstacle_collision.get(LedSpriteProperty.Y) == bird.get(LedSpriteProperty.Y)) {
             game.gameOver()
         }
     }
